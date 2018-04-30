3842172da91edb24ded59b24214802de91f59846
Lukasz Koscielny <lkoscielny.contractor@libertyglobal.com>
2018-04-30 16:22:54 +0200

    Added test with Map usage

--- src/test/java/AppRestTest.java
+++ src/test/java/AppRestTest.java
@@ -8,6 +8,7 @@ import com.jayway.restassured.specification.RequestSpecification;
 import org.testng.annotations.*;
 
 import javax.swing.text.AbstractDocument;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -45,12 +46,28 @@ public class AppRestTest {
     public void testStatusCode(){
         given().spec(spec).when().get("/posts").then().statusCode(200);
     }
-
+    @Test
+    public void testOutputSingleItem() {
+        Response response =
+                given().spec(spec).when().get("/posts/1").then().statusCode(200).extract().response();
+        responseString = response.asString();
+        Integer listaUserId = from(responseString).getInt("userId");
+        assertThat(listaUserId).isEqualTo(1);
+        Map<String, Object> mojaMapa = new HashMap<String, Object>();
+        mojaMapa.put("userId", 1);
+        mojaMapa.put("id", 1);
+        mojaMapa.put("title", "sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+        mojaMapa.put("body", "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
+        Map<String, Object> obi = from(responseString).getMap(".");
+        assertThat(obi).isEqualTo(mojaMapa);
+    }
     @Test
     public void testOutput(){
         Response response =
                 given().spec(spec).when().get("/posts").then().statusCode(200).extract().response();
         responseString = response.asString();
+        List<String> listaUserId = from(responseString).getList("userId");
+        assertThat(listaUserId).isNotEmpty();
         List<Integer> lista = from(responseString).get();
         Integer firstId = from(responseString).getInt("[0].userId");
         assertThat(firstId).isEqualTo(1);
