7915287eb544131d2c1ae092870319ddb1f0bc81
Lukasz Koscielny <lkoscielny.contractor@libertyglobal.com>
2018-04-30 15:47:21 +0200

    Added initial code and pom.xml

---
+++ src/test/java/AppRestTest.java
@@ -0,0 +1,89 @@
+import com.jayway.restassured.builder.RequestSpecBuilder;
+import com.jayway.restassured.filter.log.RequestLoggingFilter;
+import com.jayway.restassured.filter.log.ResponseLoggingFilter;
+import com.jayway.restassured.http.ContentType;
+import com.jayway.restassured.path.json.JsonPath;
+import com.jayway.restassured.response.Response;
+import com.jayway.restassured.specification.RequestSpecification;
+import org.testng.annotations.*;
+
+import javax.swing.text.AbstractDocument;
+import java.util.List;
+import java.util.Map;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.*;
+import static com.jayway.restassured.path.json.JsonPath.from;
+
+
+public class AppRestTest {
+    private static String responseString;
+    private static RequestSpecification spec;
+    @BeforeClass
+    public void setUpClass(){
+        spec = new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri("http://jsonplaceholder.typicode.com")
+                .addFilter(new ResponseLoggingFilter())//log request and response for better debugging. You can also only log if a requests fails.
+                .addFilter(new RequestLoggingFilter())
+                .build();
+
+    }
+    @AfterClass
+    public void tearDownClass(){
+
+    }
+    @BeforeMethod
+    public void setUp() {
+
+    }
+    @AfterMethod
+    public void tearDown() {
+
+    }
+    @Test
+    public void testStatusCode(){
+        given().spec(spec).when().get("/posts").then().statusCode(200);
+    }
+
+    @Test
+    public void testOutput(){
+        Response response =
+                given().spec(spec).when().get("/posts").then().statusCode(200).extract().response();
+        responseString = response.asString();
+        List<Integer> lista = from(responseString).get();
+        Integer firstId = from(responseString).getInt("[0].userId");
+        assertThat(firstId).isEqualTo(1);
+        assertThat(firstId == 1);
+        assertThat(lista).isNotEmpty();
+    }
+    @Test
+    public void testOutputString() {
+        Response response =
+                given().spec(spec).when().get("/comments?postId=1").then().statusCode(200).extract().response();
+        responseString = response.asString();
+        String email = from(responseString).getString("[2].email");
+        assertThat(email).isEqualTo("Nikita@garfield.biz");
+    }
+    @Test
+    public void testPut() {
+        String output = given().spec(spec).when().put("/posts/1").then().statusCode(200).extract().asString();
+        assertThat(output).isNotBlank();
+        assertThat(output).contains("id");
+    }
+    @Test
+    public void testPut2() {
+        Response r = given().spec(spec).when().put("/posts/1").then().statusCode(200).extract().response();
+        responseString = r.asString();
+        String id = from(responseString).getString("id");
+        assertThat(id).isEqualTo("1");
+    }
+    @Test
+    public void testGetWrongUrlDoubleSlash() {
+        given().when().get("http://jsonplaceholder.typicode.com//posts/1").then().statusCode(404);
+    }
+    @Test
+    public void testGetWrongUrl404() {
+        given().spec(spec).when().get("/posts/1/123").then().statusCode(404);
+    }
+}
